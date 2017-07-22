829bdb17b5961100fa2e9ce335f5eb17e23aea65
bo.wang <15950853008@163.com>
2017-07-22 21:59:58 +0800

    rhino-horn first commit

---
+++ src/main/java/com/chinaredstar/jedi/RhinoHornApplication.java
@@ -0,0 +1,12 @@
+package com.chinaredstar.jedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhinoHornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhinoHornApplication.class, args);
+	}
+}
