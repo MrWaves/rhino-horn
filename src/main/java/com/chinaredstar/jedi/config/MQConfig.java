864af2e0fb8c38449fed4e97287abe382ae3a133
bo.wang <bo.wang@chinaredstar.com>
2017-07-25 20:13:52 +0800

    2017.7.25  20:13

---
+++ src/main/java/com/chinaredstar/jedi/config/MQConfig.java
@@ -0,0 +1,14 @@
+package com.chinaredstar.jedi.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by bo.wang on 2017/7/25.
+ */
+@Configuration
+public class MQConfig {
+//    public static final String
+}
